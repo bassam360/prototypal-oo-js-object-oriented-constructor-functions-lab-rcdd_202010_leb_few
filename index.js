6aec8565dfba48676631ebfd8a87f857398f3352
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-26 14:59:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, x){
+  this.year = year;
+  this.color = color;
+  this.x = x
+}
+
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(address, city){
+  this.address = address;
+  this.city = city;
+}
